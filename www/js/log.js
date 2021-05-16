36a8e2d77ad082a4f270955f874f2f0cee777498
raskinfe <kinfemichaellemu@gmail.com>
2021-05-16 17:27:06 +0200

    initial commit

---
+++ www/js/log.js
@@ -0,0 +1,32 @@
+var loggedIn = false;
+
+function authenticate() {
+  var password = document.getElementById('password').value;
+  var username = document.getElementById('username').value;
+
+  loggedIn = check(password, username);
+  status();
+
+}
+
+
+function check(password, username) {
+  if (username == "" && password =="") {
+
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+function status() {
+  if (loggedIn) {
+
+    var url  ="../tabs.html";
+    window.open(url);
+  }
+  else {
+    alert("Please enter the correct values!")
+  }
+}
